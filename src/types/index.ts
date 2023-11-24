a73b9ee5fe27ce6e8eec8f9648587e3229187170
Carlos Ceagah <carlosceagah@gmail.com>
2023-11-24 16:23:47 -0300

    type: create navigation type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,19 @@
+import { NavigationProp } from '@react-navigation/native';
+type RootStackParamList = {
+  Home: undefined;
+  Slider: undefined;
+};
 
+export type TNavigationType = NavigationProp<RootStackParamList>;
+
+export interface ISlideItem {
+  item: {
+    key: string;
+    title: string;
+    text: string;
+    image: any; 
+  }
+}
 export interface IContainer {
   children: React.ReactNode;
 }
